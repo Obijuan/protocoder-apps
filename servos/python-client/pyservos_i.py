55fb2316f6a5c977ec5107980c7373974256af7f
obijuan <juan@iearobotics.com>
2015-02-23 17:21:19 +0100

    python client interactive version added

--- servos/python-client/pyservos_i.py
+++ servos/python-client/pyservos_i.py
@@ -4,6 +4,14 @@
 # (C)2015 Juan Gonzalez (Obijuan)
 # Released under the GPL license
 #-----------------------------------------------------------------------
+#-- Interactive version
+#--
+#-- Open it with  ipython pyservos_i.py -i
+#-- Then you directly can access the servos from the ipython console:
+#--  Ej.
+#--        a.pos = 80   -->  Move servo a to 80 degrees
+#--        b.pos = -50  -->  Mover servo c to -50 degrees
+
 import serial
 import Servo
 
